7bf6a0ab1420b94434dd129146974a430f337885
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2025-07-26 13:43:18 +0530

    Fixed navigation

--- client/src/components/Participate.tsx
+++ client/src/components/Participate.tsx
@@ -54,9 +54,7 @@ const ThemeSection = () => {
 
                 <div className="lg:text-left">
                     <a
-                        href="https://www.instagram.com/p/DL5NPqRo4wb/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
-                        target="_blank"
-                        rel="noopener noreferrer"
+                        href="/gallery"
                         className="fancy text-xl border mb-5 border-neutral/70 rounded-full px-8 py-6 lg:px-20 lg:py-8 hover:bg-white duration-200 inline-block font-semibold"
                     >
                         VIEW RESULTS →
